f0034a7f005195655d0dac18aa76fff616674e28
Olga Toptunow <o.apiecionek@gmail.com>
2022-06-20 00:04:43 +0200

    Header component

---
+++ app/src/components/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Header.css"
+
+export default function Header(){
+    return (
+        <div className="header">
+            <div className="logo">
+            <img className="logo" src="/images/logo.png" alt="logo" />
+            <p>MemeGenerator</p>
+            </div>
+            <p>Bazinga</p>
+
+
+        </div>
+    )
+}
