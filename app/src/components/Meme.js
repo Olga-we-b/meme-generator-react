2e6e80200af68de0f9be244167a94f5e9c208dfa
Olga Toptunow <o.apiecionek@gmail.com>
2022-06-22 21:23:01 +0200

    randomized pictures

--- app/src/components/Meme.js
+++ app/src/components/Meme.js
@@ -1,22 +1,37 @@
-import React from "react";
+import React, {useState} from "react";
 import './Meme.css';
-
+import memesData from "../memesData";
 export default function Meme(){
+    const [url, setUrl] =  useState('https://i.imgflip.com/cv1y0.jpg')
+
+
+    function getMemeImage(){
+        const memesArray = memesData.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        const url = memesArray[randomNumber].url
+        setUrl(() => url)
+        console.log(url)
+        return url
+    }
+
     return(
         <main>
-            <form>
                 <div className="inputs">
                     <input type="text" placeholder="Top Text"></input>
                     <input type="text" placeholder="Bottom Text"></input>
                 </div>
                 <div className="custom-button">
                     <div>
-                    <button>Get a new image </button>
+                    <button onClick={getMemeImage}>Get a new image </button>
                     </div>
                     <p>Get a new image 🖼</p>
                 </div>
-                
-            </form>
+
+                <div className="picture-frame">
+                    <img src={url}  alt="meme-picture"></img>
+                </div>
+
+
         </main>
         )
     }
